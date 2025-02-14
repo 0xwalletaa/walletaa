59711fca13044a0caaff9898e3c598ea6c5fa04c
0xZPL <326486019@qq.com>
2025-02-14 20:47:55 +0800

    get_factory_and_paymaster

--- entry_point/get_src.py
+++ entry_point/get_src.py
@@ -18,6 +18,7 @@ except:
 
 # ETH 1 # OP 10 # Scroll 534352 # Polygon zkEVM 1101 # Arbitrum One 42161 # Base 8453 # BSC 56 # Polygon Mainnet 137 # Fantom Opera 250 # Avalanche C-Chain 43114 # Celo 42220 # Gnosis 100
 chainids = [1, 10, 534352, 1101, 42161, 8453, 56, 137, 250, 43114, 42220, 100] 
+chainids.sort()
 
 address = "0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789"
 
@@ -54,6 +55,7 @@ for chainid in chainids:
         print(filename, lastlen)
         
         fromBlock = int(data["result"][-1]["blockNumber"], 16)
+    time.sleep(1)
 
 address = "0x0000000071727de22e5e9d8baf0edac6f37da032"
 
--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ python3 get_list.py
 cd entry_point
 python3 get_src.py
 python3 get_account_deployed.py
+python3 get_factory_and_paymaster.py
