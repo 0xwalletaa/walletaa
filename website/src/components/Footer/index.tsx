9c0c879f8f3b41cdfa5a4feef1815c93a35f652f
0xZPL <326486019@qq.com>
2025-05-07 00:33:36 +0800

    fix title

--- website/src/components/Footer/index.tsx
+++ website/src/components/Footer/index.tsx
@@ -10,21 +10,21 @@ const Footer: React.FC = () => {
       }}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: 'WalletAA',
+          title: 'WalletAA',
+          href: 'https://walletaa.com',
           blankTarget: true,
         },
         {
           key: 'github',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/0xwalletaa/',
           blankTarget: true,
         },
         {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
+          key: '0xWalletAA',
+          title: '0xWalletAA',
+          href: 'https://github.com/0xwalletaa/',
           blankTarget: true,
         },
       ]}
