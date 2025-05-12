13f67927328c4743f3f5a63698700bd6c2b1073a
0xZPL <326486019@qq.com>
2025-05-12 23:24:42 +0800

    add bsc rpc

--- backend/get_bsc.sh
+++ backend/get_bsc.sh
@@ -1,1 +1,15 @@
-python3 get_block.py --name bsc --endpoints https://api.zan.top/bsc-mainnet https://endpoints.omniatech.io/v1/bsc/mainnet/public https://bsc-mainnet.public.blastapi.io https://bsc.drpc.org https://bsc-dataseed4.bnbchain.org --start_block 49530000
+python3 get_block.py --name bsc --endpoints \
+https://api.zan.top/bsc-mainnet \
+https://endpoints.omniatech.io/v1/bsc/mainnet/public \
+https://bsc-mainnet.public.blastapi.io \
+https://bsc.drpc.org \
+https://bsc-dataseed4.bnbchain.org \
+https://binance.llamarpc.com \
+https://bnb.rpc.subquery.network/public \
+https://bsc-mainnet.public.blastapi.io \
+https://bsc.meowrpc.com \
+https://rpc-bsc.48.club \
+https://rpc.therpc.io/bsc \
+https://bsc.rpc.blxrbdn.com \
+https://bsc.blockrazor.xyz \
+--start_block 49530000
