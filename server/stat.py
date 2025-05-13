ac3b0b47abc458a0019b27bdfeb83af622b22900
0xZPL <326486019@qq.com>
2025-05-13 18:38:28 +0800

    time calc

--- server/stat.py
+++ server/stat.py
@@ -1,7 +1,13 @@
 import util
+import time
 
 util.NAME = "mainnet"
+
+start_time = time.time()
 
 txs = util.get_all_type4_txs()
 
 print(len(txs))
+
+end_time = time.time()
+print(f"Time taken: {end_time - start_time} seconds")
