2a3960e94c7d2210a603a7d6f02b54930f3f08c4
0xZPL <326486019@qq.com>
2025-05-14 19:30:37 +0800

    fix rpc

--- backend/get_bsc.sh
+++ backend/get_bsc.sh
@@ -1,7 +1,6 @@
 while true; do
 python3 get_block.py --name bsc --endpoints \
 https://api.zan.top/bsc-mainnet \
-https://endpoints.omniatech.io/v1/bsc/mainnet/public \
 https://bsc-mainnet.public.blastapi.io \
 https://bsc.drpc.org \
 https://bsc-dataseed4.bnbchain.org \
@@ -10,7 +9,6 @@ https://bnb.rpc.subquery.network/public \
 https://bsc-mainnet.public.blastapi.io \
 https://bsc.meowrpc.com \
 https://rpc-bsc.48.club \
-https://rpc.therpc.io/bsc \
 https://bsc.rpc.blxrbdn.com \
 https://bsc.blockrazor.xyz \
 --num_threads 10 \
