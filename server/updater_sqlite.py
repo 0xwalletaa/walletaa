9210c6ed880c652483cc511dc266149736ba47ed
0xZPL <326486019@qq.com>
2025-06-30 01:53:14 +0800

    fix wrong type4_tx 2

--- server/updater_sqlite.py
+++ server/updater_sqlite.py
@@ -206,6 +206,7 @@ def update_info_by_block(info_db_path, block_db_path):
     if wrong_block_number > 0:
         print(f"Wrong block number: {wrong_block_number}")
         block_tx_cursor.execute("DELETE FROM type4_transactions WHERE block_number = ?", (wrong_block_number,))
+        block_timestamp_cursor.execute("DELETE FROM blocks WHERE block_number = ?", (wrong_block_number,))
         block_conn.commit()
 
     info_conn.close()
