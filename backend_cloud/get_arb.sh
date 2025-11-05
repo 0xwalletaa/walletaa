092aa97df1eded716067215c6ac7fc7ff3004158
0xZPL <326486019@qq.com>
2025-11-05 23:24:27 +0800

    arb rpc

--- backend_cloud/get_arb.sh
+++ backend_cloud/get_arb.sh
@@ -1,16 +1,21 @@
 python3 get_block.py --name arb --endpoints \
-https://arbitrum.rpc.subquery.network/public \
-https://arbitrum.meowrpc.com \
-https://arbitrum.gateway.tenderly.co \
-https://rpc.therpc.io/arbitrum \
-https://arbitrum.drpc.org \
+https://rpc.owlracle.info/arb/70d38ce1826c4a60bb2a8e05a6c8b20f \
+https://arbitrum-one.rpc.grove.city/v1/01fdb492 \
+https://arbitrum-one-public.nodies.app \
 https://arbitrum-one-rpc.publicnode.com \
-https://arb-pokt.nodies.app \
+https://arbitrum.drpc.org \
+https://rpc.poolz.finance/arbitrum \
+https://arbitrum.public.blockpi.network/v1/rpc/public \
+https://public-arb-mainnet.fastnode.io \
+https://arbitrum.rpc.subquery.network/public \
+https://arb-one-mainnet.gateway.tatum.io \
 https://arb1.lava.build \
+https://api.zan.top/arb-one \
+https://arbitrum.gateway.tenderly.co \
+https://arb1.arbitrum.io/rpc \
 https://arbitrum-one.public.blastapi.io \
 https://1rpc.io/arb \
-https://arb1.arbitrum.io/rpc \
---num_threads 10 \
+--num_threads 15 \
 --start_block 378200000 \
 --block_db_path /mnt
 
