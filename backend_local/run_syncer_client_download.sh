886c67651d298680b781aab54b3026bd2014d163
0xZPL <326486019@qq.com>
2026-03-23 04:23:34 +0800

    update syncer

--- backend_local/run_syncer_client_download.sh
+++ backend_local/run_syncer_client_download.sh
@@ -3,16 +3,16 @@ echo "Starting sync..."
 echo ""
 
 # Sync mainnet
-python3 syncer_client.py --name mainnet --server_url "$SERVER_URL" --start_block 24180000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name bsc --server_url "$SERVER_URL" --start_block 74300000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name op --server_url "$SERVER_URL" --start_block 146000000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name arb --server_url "$SERVER_URL" --start_block 418000000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name base --server_url "$SERVER_URL" --start_block 40500000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name bera --server_url "$SERVER_URL" --start_block 15300000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name gnosis --server_url "$SERVER_URL" --start_block 44000000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name ink --server_url "$SERVER_URL" --start_block 34200000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name uni --server_url "$SERVER_URL" --start_block 37000000 --db_path "$DB_PATH" --download
-python3 syncer_client.py --name scroll --server_url "$SERVER_URL" --start_block 27700000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name mainnet --server_url "$SERVER_URL" --start_block 24710000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name bsc --server_url "$SERVER_URL" --start_block 88130000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name op --server_url "$SERVER_URL" --start_block 149300000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name arb --server_url "$SERVER_URL" --start_block 444500000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name base --server_url "$SERVER_URL" --start_block 43700000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name bera --server_url "$SERVER_URL" --start_block 18590000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name gnosis --server_url "$SERVER_URL" --start_block 45280000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name ink --server_url "$SERVER_URL" --start_block 40710000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name uni --server_url "$SERVER_URL" --start_block 41360000 --db_path "$DB_PATH" --download
+python3 syncer_client.py --name scroll --server_url "$SERVER_URL" --start_block 32370000 --db_path "$DB_PATH" --download
 
 echo ""
 echo "Sync download completed!"
